394f1fb46cfa19501ed5d9d00facf4efc64bbe02
Adrián Chávez <adri-142@hotmail.com>
2023-03-23 15:43:50 -0300

    First Commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+// server used to send send emails
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/", router);
+
+const port = process.env.PORT;
+app.listen(port, () => console.log("Server Running at port " + port));
+
+const contactEmail = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
+
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to Send");
+  }
+});
+
+router.get("/", (req, res) => {
+  res.status(200).send("<h1>Home</h1>");
+});
+
+router.post("/contact", (req, res) => {
+  const name = req.body.firstName + " " + req.body.lastName;
+  const email = req.body.email;
+  const message = req.body.message;
+  const phone = req.body.phone;
+  const mail = {
+    from: name,
+    to: "adri@gmail.com",
+    subject: "Contact Form Submission - Portfolio",
+    html: `<p>Name: ${name}</p>
+           <p>Email: ${email}</p>
+           <p>Phone: ${phone}</p>
+           <p>Message: ${message}</p>`,
+  };
+
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json({ code: 200, status: "Message Sent" });
+    }
+  });
+});
